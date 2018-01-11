8028886b278e5d17ee9438c9031ec320449247a3
sapan <sapantanted99@gmail.com>
2018-01-11 23:26:34 +0530

    working smoothly

--- MongoToTimescaleScript.py
+++ MongoToTimescaleScript.py
@@ -2,13 +2,14 @@ import datetime
 import json
 import os
 import os.path
-import pymongo as pm
 import smtplib
 import sys
 import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from mysql.connector import (connection)
+
+import psycopg2
+import pymongo as pm
 
 
 # "power_k_lh_a",
@@ -75,7 +76,11 @@ def connect_mongo():
     return db_mo, con
 
 
-def update_transfered_records_log(table_name, str_backuped_till, records_copied, mysql_table_name):
+def connect_timescaledb(ts_db_name, ts_db_user, ts_db_pass, ts_db_host):
+    return psycopg2.connect(database=ts_db_name, user=ts_db_user, password=ts_db_pass, host=ts_db_host)
+
+
+def update_transfered_records_log(table_name, str_backuped_till, records_copied, timescale_table_name):
     dict = {table_name: str_backuped_till}
     if os.path.exists(transfered_records_log):
         file = open(transfered_records_log, 'r')
@@ -90,9 +95,9 @@ def update_transfered_records_log(table_name, str_backuped_till, records_copied,
     file.flush()
     file.close()
     print("Records till: (" + table_name + ")=>" + str_backuped_till + " inserted, no. of records: " + str(
-        records_copied) + " to mysql table: " + mysql_table_name)
+        records_copied) + " to timescale table: " + timescale_table_name)
     save_log("Records till: (" + table_name + ")=>" + str_backuped_till + " inserted, no. of records: " + str(
-        records_copied) + " to mysql table: " + mysql_table_name + "\n")
+        records_copied) + " to timescale table: " + timescale_table_name + "\n")
 
 
 def fetch_from(table_name):
@@ -107,16 +112,17 @@ def fetch_from(table_name):
 def fetch_till(ts_fetch_from):
     return ts_fetch_from + records_batch_size
 
+
 # -----------------------------------------------------------
 config_fp = open("config.json", 'r')
 config = json.load(config_fp)
 records_batch_size = config['records_batch_size']
 mongo_host = config['mongo_host']
-mysql_user = config['mysql_user']
-mysql_pass = config['mysql_pass']
-mysql_host = config['mysql_host']
-mysql_db_name = config['mysql_db_name']
-mysql_tables = config['mysql_tables']
+timescale_user = config['timescale_user']
+timescale_pass = config['timescale_pass']
+timescale_host = config['timescale_host']
+timescale_db_name = config['timescale_db_name']
+timescale_tables = config['timescale_tables']
 backup_from = config['backup_from']
 backup_till = config['backup_till']
 schema = config['schema']
@@ -128,25 +134,25 @@ channelwise_tables = config['channelwise_tables']
 # -----------------------------------------------------------
 
 save_log("-------------------------------------------------------")
-save_log("script started at "+str_from_timestamp(time.time())+"\n")
+save_log("script started at " + str_from_timestamp(time.time()) + "\n")
 try:
     mongo_db, mongo_con = connect_mongo()  # connection to mongo db
 except:
     print("Unexpected error In Mongo Connection:", sys.exc_info()[0])
     tFile = str(time.strftime("%d-%m-%Y"))
     tWrite = time.strftime("%H:%M:%S", time.localtime(time.time()))
-    with open("./mongotomysqlscripterrror/error_" + tFile, "a+") as errF:
+    with open("./mongototimescalescripterrror/error_" + tFile, "a+") as errF:
         errF.write(str(tWrite) + "\n" + str(sys.exc_info()[0]))
 
-mysql_con = connection.MySQLConnection(user=mysql_user, password=mysql_pass, host=mysql_host, db=mysql_db_name)
-cursor = mysql_con.cursor()
+timescale_con = connect_timescaledb(timescale_db_name, timescale_user, timescale_pass, timescale_host)
+cursor = timescale_con.cursor()
 try:
     for channel in channelwise_tables:
         tables = channelwise_tables[channel]
         for table_name in tables:
             ts_fetch_from = ts_backup_from
             ts_fetch_till = ts_backup_from
-            mysql_con.rollback()
+            timescale_con.rollback()
             while ts_fetch_till < ts_backup_till:
                 select_cols = schema[channel]
                 ts_fetch_from = fetch_from(table_name)
@@ -155,15 +161,15 @@ try:
                     ts_fetch_till = ts_backup_till
                 mongo_query = {"$query": {"TS": {"$gte": ts_fetch_from, "$lt": ts_fetch_till}}, "$orderby": {"TS": 1}}
                 print("------------------------------------------")
-                print("coping from: " + channel + "-" + table_name + " to " + mysql_tables[channel])
+                print("coping from: " + channel + "-" + table_name + " to " + timescale_tables[channel])
                 rows = mongo_con[table_name].find(mongo_query, select_cols)
                 records_to_be_copied = 0
                 col_str = ""
                 for col in schema[channel]:
-                    col_str += "`" + col + "`,"
-                col_str += "`sensor_id`"
-                blank_mysql_query = "insert into `" + str(mysql_tables[channel]) + "` (" + col_str + ") values "
-                filled_mysql_query = blank_mysql_query
+                    col_str += " " + col + ","
+                col_str += "sensor_id"
+                blank_timescale_query = "insert into " + str(timescale_tables[channel]) + " (" + col_str + ") values "
+                filled_timescale_query = blank_timescale_query
                 print("started building query!")
                 for row in rows:
                     value_str = ""
@@ -171,39 +177,42 @@ try:
                         if row[col] is None:
                             value = "NULL"
                         elif float(row[col]) == float('inf'):
-                            value = "'-1'"
+                            value = "-1"
                         else:
-                            value = "'" + str(row[col]) + "'"
+                            if col == "TS" or col == "TS_RECV":
+                                value = "to_timestamp(" + str(row[col]) + ")"
+                            else:
+                                value = "" + str(row[col]) + ""
                         value_str += " " + str(value) + ","
                     value_str += "'" + table_name + "'"
-                    filled_mysql_query += " (" + value_str + "),"
+                    filled_timescale_query += " (" + value_str + "),"
                     records_to_be_copied += 1
                 print("ended building query! no. of records: " + str(records_to_be_copied))
-                filled_mysql_query = filled_mysql_query[:-1]
+                filled_timescale_query = filled_timescale_query[:-1]
                 print("removed comma from last!")
                 if records_to_be_copied > 0:
                     try:
-                        cursor.execute(filled_mysql_query)
-                        # print(filled_mysql_query)
+                        # save_log(filled_timescale_query)
+                        cursor.execute(filled_timescale_query)
                     except Exception as e:
                         records_to_be_copied = 0
                         print("exception during query execution: ", e)
                         save_log("collection: " + table_name + " " + str(e) + "\n")
-                        mysql_con.rollback()
+                        timescale_con.rollback()
                         break
                     print("executed query!")
-                    mysql_con.commit()
+                    timescale_con.commit()
                     print("commited query!")
                 update_transfered_records_log(table_name, str_from_timestamp(ts_fetch_till), records_to_be_copied,
-                                              mysql_tables[channel])
-                print("no. of records copied: " + str(records_to_be_copied) + " into mysql table: " + str(
-                    mysql_tables[channel]))
+                                              timescale_tables[channel])
+                print("no. of records copied: " + str(records_to_be_copied) + " into timescale table: " + str(
+                    timescale_tables[channel]))
 
 except Exception as e:
     print("outer exception: ", e)
     save_log(str(e) + "\n")
-    report_error("sapantanted99@gmail.com", "mongo to mysql script stopped", "outer exception: " + str(e) + "\n")
+    report_error("sapantanted99@gmail.com", "mongo to timescale script stopped", "outer exception: " + str(e) + "\n")
 finally:
-    mysql_con.close()
+    timescale_con.close()
 
-save_log("script ended at "+str_from_timestamp(time.time())+"\n")
+save_log("script ended at " + str_from_timestamp(time.time()) + "\n")
